57b07af9affcb65406b5be2a6dfc388ddf27a725
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-01-08 12:20:25 -0600

    function testing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,5 +5,11 @@ let basket = [];  //initialize array to hold items
 
 function addItem(item) {
   basket.push(item); // adds value input(item) to the end of array
+  console.log(`${item} was added to your Basket!`);
   return true;
 }
+
+//testing function
+console.log(addItem('Ramen')); //console should log true
+console.log(addItem('Carrots'));
+console.log(addItem('Popcorn'));
