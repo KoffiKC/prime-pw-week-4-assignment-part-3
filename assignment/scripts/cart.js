87b9e91339cc3c8d94e96807d6aab31525ef5759
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-01-08 11:55:11 -0600

    basket array and addItem function created and working

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,9 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];  //initialize array to hold items
+
+function addItem(item) {
+  basket.push(item); // adds value input(item) to the end of array
+  return true;
+}
